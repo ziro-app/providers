d50b7f98c050d9af1f83bda5e3aeadfc32710728
unknown <ahmad.forhat@gmail.com>
2020-01-27 13:35:04 -0300

    Desmembrando a função e criando um main de apoio

--- functions/listResumeFilter.js
+++ functions/listResumeFilter.js
@@ -1,10 +1,4 @@
-const rp = require('request-promise-native');
-const optionsGet = require('./apoio/optionsGetGoogle')
-const listarApelidos = require('./apoio/listarApelidos')
-const listarComissoesAssessores2 = require('./apoio/comissoes/asessores2')
-const listarComissoesProspeccao = require('./apoio/comissoes/prospeccao1')
-const listarCobranca1 = require('./apoio/comissoes/cobranca1')
-const listarLojista1 = require('./apoio/comissoes/lojista1')
+const main = require('./apoio/main')
 require('dotenv').config()
 
 
@@ -16,19 +10,12 @@ exports.handler = function(event, context, callback){
         })
     }
     const getEmployees = async () => {
-        const dataBaseAssessores = await rp(optionsGet("'Apoio Comissões Assessores'!A:E"))
-        const dataBaseResto = await rp(optionsGet("'Apoio Comissões'!A:E"))
-        const dataBaseFuncionarios = await rp(optionsGet("'Base Funcionários'!A:V"))
-        const listaBaseFuncionarios = await listarApelidos(dataBaseFuncionarios,event.queryStringParameters.mes,event.queryStringParameters.ano)
-        const comissoesAssessores2 = await listarComissoesAssessores2(listaBaseFuncionarios,dataBaseAssessores,event.queryStringParameters.mes,event.queryStringParameters.ano)
-        const comissoesProspeccao1 = await listarComissoesProspeccao(comissoesAssessores2,dataBaseResto,event.queryStringParameters.mes,event.queryStringParameters.ano)
-        const comissaoCobranca1 = await listarCobranca1(comissoesProspeccao1,dataBaseResto,event.queryStringParameters.mes,event.queryStringParameters.ano)
-        const comissaoLojista1 = await listarLojista1(comissaoCobranca1,dataBaseResto,event.queryStringParameters.mes,event.queryStringParameters.ano)
+        const resultado = await main(event.queryStringParameters.mes,event.queryStringParameters.ano)
         try {
             console.log(comissaoLojista1)
-            send(comissaoLojista1)
+            send(resultado)
         } catch (error) {
-            send(error)
+            send(resultado)
         }
     }
     if(event.httpMethod == 'GET' && event.headers.authorization == process.env.basicAuth){
