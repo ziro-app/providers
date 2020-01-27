d4e5e2b9b2d011c4dc13e6beb63871a8c4d34185
unknown <ahmad.forhat@gmail.com>
2020-01-27 13:04:36 -0300

    Adição dos calculos de comissão referentes ao setor de lojistica

---
+++ functions/apoio/comissoes/lojista1.js
@@ -0,0 +1,24 @@
+const calculoNovo = require("./apoio/calculoNovo")
+const calculoAntigo = require("./apoio/calculoAntigo")
+
+const lojista1 = (data, dataCalculo, mes, ano) => {
+    const novo = calculoNovo(dataCalculo,mes,ano).replace(",",".")
+    const antigo = calculoAntigo(dataCalculo,mes,ano).replace(",",".")
+    const calculo = (novo, antigo) => {
+        const calculo =  Number(novo)*0.005 + Number(antigo)*0.005
+        return calculo
+    }
+    let resultado = []
+    for(i=0; i < data.values.length; i++){
+        if(data.values[i][4] == "Lojista1" && ano == 2019 && mes > 8 || data.values[i][4] == "Lojista1" && ano > 2019){
+            resultado.push([data.values[i][0],data.values[i][1],data.values[i][2],data.values[i][3], Math.round(calculo(novo,antigo)*100)/100, data.values[i][4]])
+        }else if(data.values[i][5] == null){
+            resultado.push([data.values[i][0],data.values[i][1],data.values[i][2],data.values[i][3],0,data.values[i][4]])
+        }else{
+            resultado.push([data.values[i][0],data.values[i][1],data.values[i][2],data.values[i][3],data.values[i][4],data.values[i][5]])
+        }
+    }
+    return {"values": resultado,            }
+}
+
+module.exports = lojista1
