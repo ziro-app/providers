a8544cea39c2349f2c8b793fce642378ff8399bc
unknown <ahmad.forhat@gmail.com>
2020-01-27 16:49:55 -0300

    Melhorando funcionalidade do codigo

--- functions/apoio/main.js
+++ functions/apoio/main.js
@@ -11,11 +11,11 @@ const main = async (mes, ano) => {
     const dataBaseAssessores = await rp(optionsGet("'Apoio Comissões Assessores'!A:E"))
     const dataBaseResto = await rp(optionsGet("'Apoio Comissões'!A:E"))
     const dataBaseFuncionarios = await rp(optionsGet("'Base Funcionários'!A:V"))
-    const listaBaseFuncionarios = await listarApelidos(dataBaseFuncionarios,mes,ano)
-    const comissoesAssessores2 = await listarComissoesAssessores2(listaBaseFuncionarios,dataBaseAssessores,mes,ano)
-    const comissoesProspeccao1 = await listarComissoesProspeccao(comissoesAssessores2,dataBaseResto,mes,ano)
-    const comissaoCobranca1 = await listarCobranca1(comissoesProspeccao1,dataBaseResto,mes,ano)
-    const comissaoLojista1 = await listarLojista1(comissaoCobranca1,dataBaseResto,mes,ano)
+    const listaBaseFuncionarios = listarApelidos(dataBaseFuncionarios,mes,ano)
+    const comissoesAssessores2 = listarComissoesAssessores2(listaBaseFuncionarios,dataBaseAssessores,mes,ano)
+    const comissoesProspeccao1 = listarComissoesProspeccao(comissoesAssessores2,dataBaseResto,mes,ano)
+    const comissaoCobranca1 = listarCobranca1(comissoesProspeccao1,dataBaseResto,mes,ano)
+    const comissaoLojista1 = listarLojista1(comissaoCobranca1,dataBaseResto,mes,ano)
     try {
         return comissaoLojista1
     } catch (error) {
