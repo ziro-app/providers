bf8945956903d58c081dff1da23b2210914459fd
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-01-27 15:18:34 -0800

    fix test

--- test.js
+++ test.js
@@ -1,43 +1,54 @@
 const test = require('ava')
 const assessores2 = require('./functions/apoio/comissoes/asessores2')
 
+// const assessor = {
+// 	values: [[
+// 		'Rúbia da Conceição Almeida Goncalves',
+// 		'Rubia',
+// 		'27/03/1975',
+// 		'rubia.ziro@gmail.com',
+// 		'rubida@ziromoda.com.br',
+// 		'+55 (11) 98292-1481',
+// 		'+55 (11) 98292-1481',
+// 		'2000,00',
+// 		'Sim',
+// 		'Assessor',
+// 		'386.856.038-62',
+// 		'29.806.516-2',
+// 		'SSP-SP',
+// 		'21/10/2002',
+// 		'Casada',
+// 		'Rua Mirador, 61,  Guarulhos, SP. CEP. 07243-390',
+// 		'2015-01-01',
+// 		'Ewertton (Filho) 98188-5155 Leo (Marido) 95972-2364',
+// 		'Assessor2'
+// 	]]
+// }
+
 const assessor = {
 	values: [[
-		'Rúbia da Conceição Almeida Goncalves',
+		12,
+		2019,
 		'Rubia',
-		'27/03/1975',
-		'rubia.ziro@gmail.com',
-		'rubida@ziromoda.com.br',
-		'+55 (11) 98292-1481',
-		'+55 (11) 98292-1481',
 		'2000,00',
-		'Sim',
-		'Assessor',
-		'386.856.038-62',
-		'29.806.516-2',
-		'SSP-SP',
-		'21/10/2002',
-		'Casada',
-		'Rua Mirador, 61,  Guarulhos, SP. CEP. 07243-390',
-		'2015-01-01',
-		'Ewertton (Filho) 98188-5155 Leo (Marido) 95972-2364',
 		'Assessor2'
 	]]
 }
 
 const comissao = {
 	values: [[
-			'1',
+			'12',
 			'2019',
 			'Rubia',
-			'21280,08',
+			'9327,18',
 			'R$ 212,80'
 		]]
 }
 
 test('Comissão Assessor', t => {
-	const value = assessores2(assessor, comissao, 1, 2019)
-	const comissionCalculated = value.values[0][3]
-	const comissionExpected = 2086.79
+	const value = assessores2(assessor, comissao, 12, 2019)
+	console.log(value)
+	const comissionCalculated = value.values[0][4]
+	const comissionExpected = 932.7
 	t.is(comissionCalculated, comissionExpected)
 })
