fde950a06d8b310864e1bb82a2235c0a21fb3de4
unknown <ahmad.forhat@gmail.com>
2020-01-28 00:48:47 -0300

    arrumando condicional de comissão bonus para assessores e data do mesmo

--- functions/apoio/comissoes/asessores2.js
+++ functions/apoio/comissoes/asessores2.js
@@ -9,20 +9,20 @@ const assessor2 = (data,dataBaseAssessores, mes,ano) => {
     let resultado1 = []
     for(i=0; i < dataBaseAssessores.values.length; i++){
         if(dataBaseAssessores.values[i][0] == mes && dataBaseAssessores.values[i][1] == ano){
-            resultado1.push(dataBaseAssessores.values[i][0],dataBaseAssessores.values[i][1],dataBaseAssessores.values[i][2],dataBaseAssessores.values[i][3])
+            resultado1.push(dataBaseAssessores.values[i][0],dataBaseAssessores.values[i][1],dataBaseAssessores.values[i][2],dataBaseAssessores.values[i][3],dataBaseAssessores.values[i][4])
         }
     }
     let resultado =  []
         for(i=0; i< data.values.length; i++){
             let index = resultado1.indexOf(data.values[i][2])
-            if(index !== -1 && ano == 2019 && mes > 8 && data.values[i][2] && data.values[i][4] == "Assessor2" || index !== -1 && ano > 2019 && data.values[i][4] == "Assessor2" ){
-                if(Math.round(Number(resultado1[index+1].replace(",","."))) >= 400000){
+            if(index !== -1 && ano > 2019 && data.values[i][4] == "Assessor2" ){
+                if(Math.round(Number(resultado1[index+2].replace(",","."))) >= 400000){
                     resultado.push([data.values[i][0],data.values[i][1],data.values[i][2],Number(data.values[i][3].replace(",",".")),(Math.round(Number(resultado1[index+1].replace(",",".")))*10/100)+1400,data.values[i][4]])
-                }else if(Math.round(Number(resultado1[index+1].replace(",","."))) >= 300000){
+                }else if(Math.round(Number(resultado1[index+2].replace(",","."))) >= 300000){
                     resultado.push([data.values[i][0],data.values[i][1],data.values[i][2],Number(data.values[i][3].replace(",",".")),(Math.round(Number(resultado1[index+1].replace(",",".")))*10/100)+1200,data.values[i][4]])
-                }else if(Math.round(Number(resultado1[index+1].replace(",","."))) >= 200000){
+                }else if(Math.round(Number(resultado1[index+2].replace(",","."))) >= 200000){
                     resultado.push([data.values[i][0],data.values[i][1],data.values[i][2],Number(data.values[i][3].replace(",",".")),(Math.round(Number(resultado1[index+1].replace(",",".")))*10/100)+900,data.values[i][4]])
-                }else if(Math.round(Number(resultado1[index+1].replace(",","."))) >= 100000){
+                }else if(Math.round(Number(resultado1[index+2].replace(",","."))) >= 100000){
                     resultado.push([data.values[i][0],data.values[i][1],data.values[i][2],Number(data.values[i][3].replace(",",".")),(Math.round(Number(resultado1[index+1].replace(",",".")))*10/100)+500,data.values[i][4]])
                 }else{
                     resultado.push([data.values[i][0],data.values[i][1],data.values[i][2],Number(data.values[i][3].replace(",",".")),(Math.round(Number(resultado1[index+1].replace(",",".")))*10/100)+0,data.values[i][4]])
--- test.js
+++ test.js
@@ -26,7 +26,7 @@ const assessor = {
 }
 
 const comissao = {
-	values: [[
+	values: [[	
 			'1',
 			'2019',
 			'Rubia',
