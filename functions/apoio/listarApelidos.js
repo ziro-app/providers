a3f7c22f7a7febfc7adfcbaca2cddf97b8146db7
unknown <ahmad.forhat@gmail.com>
2020-01-28 16:20:35 -0300

    fazendo função if else para listar todos os funcionários se não tiver parâmetros de ano e mes na query

--- functions/apoio/listarApelidos.js
+++ functions/apoio/listarApelidos.js
@@ -1,15 +1,33 @@
 const listarApelidos = (data, mes, ano) => {
-    let arrayApelidos = []
-            for(i=1; i < data.values.length; i++){
+    if(mes != undefined || ano != undefined){
+        let arrayApelidos = []
+            for(i=0; i < data.values.length; i++){
                 if(data.values[i][18] == "-" && new Date(data.values[i][17]) <= new Date(`${ano}-${mes}-28`)){
                     arrayApelidos.push([ano,mes,data.values[i][1],data.values[i][7],data.values[i][21]])
                 }
                  else if(new Date(data.values[i][18]) >= new Date(`${ano}-${mes}-28`) && new Date(data.values[i][17]) <= new Date(`${ano}-${mes}-28`)){
                     arrayApelidos.push([ano,mes,data.values[i][1],data.values[i][7],data.values[i][21]])
                 }
-    }
-        return {
-           "values": arrayApelidos
+                return {
+                   "values": arrayApelidos
+                }
+            }
+        }else{
+            let arrayApelidos = []
+            for(listar of data.values){
+                for(n=2018; n <= new Date().getFullYear(); n++){
+                    for(i=1; i <= 12; i++){
+                            if(listar[18] == "-" && new Date(`${n}-${i}-28`) >= new Date(listar[17])){
+                                arrayApelidos.push([n,i,listar[1],listar[7],listar[21]])
+                            }else if(new Date(`${n}-${i}-28`) >= new Date(listar[17]) && new Date(listar[18]) >= new Date(`${n}-${i}-01`)){
+                                arrayApelidos.push([n,i,listar[1],listar[7],listar[21]])
+                            }
+                    }
+                }
+            }
+            return {
+                "values": arrayApelidos
+            }
         }
 }
 
