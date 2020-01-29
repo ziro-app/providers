b45ec1cb3ead0dbd2f8863f1e7e3360f7ffb88b0
unknown <ahmad.forhat@gmail.com>
2020-01-29 11:15:15 -0300

    loop dos valores para construir base de dados das comissões por assessor, mes e ano

--- functions/apoio/comissoes/asessores2.js
+++ functions/apoio/comissoes/asessores2.js
@@ -8,6 +8,7 @@
 const transNumb = (numero) => {
     return Number(numero.replace(",","."))
         }
+
 const assessor2020 = (transacionado_mes, receita_mes) => {
     const base = receita_mes * 0.1;
     if (transacionado_mes >= 400000) return Math.round((base + 1400)*100)/100
@@ -16,6 +17,12 @@ const assessor2020 = (transacionado_mes, receita_mes) => {
     if (transacionado_mes >= 100000) return Math.round((base + 500)*100)/100
     else return Math.round(base*100)/100
   };
+
+const assessor2 = (data) => {
+    let resultado = []
+    for(comissoes2020 of data.values){
+        if(comissoes2020[1] >= 2020){
+            resultado.push({"ano": comissoes2020[1],"mes": comissoes2020[0],"apelido": comissoes2020[2],"parcela2":assessor2020(transNumb(comissoes2020[4]), transNumb(comissoes2020[3]))})
             }
         }
 
