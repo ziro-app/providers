f93b7424149c6f9bfe379065148e1071d9e47dda
unknown <ahmad.forhat@gmail.com>
2020-01-28 21:25:42 -0300

    adicionando condicional de >= 2019

--- functions/apoio/listarApelidos.js
+++ functions/apoio/listarApelidos.js
@@ -2,7 +2,7 @@ const listarApelidos = (data, mes, ano) => {
     if(mes != undefined || ano != undefined){
         let arrayApelidos = []
             for(let i=0; i < data.values.length; i++){
-                if(data.values[i][18] == "-" && new Date(data.values[i][17]) <= new Date(`${ano}-${mes}-28`)){
+                if(data.values[i][18] == "-" && new Date(data.values[i][17]) <= new Date(`${ano}-${mes}-28` && ano >= 2019)){
                     arrayApelidos.push([ano,mes,data.values[i][1],data.values[i][7],data.values[i][21]])
                 }
                  else if(new Date(data.values[i][18]) >= new Date(`${ano}-${mes}-28`) && new Date(data.values[i][17]) <= new Date(`${ano}-${mes}-28`)){
