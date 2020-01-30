f68bee3382eb8e5f24c617cbc443f203ae755360
unknown <ahmad.forhat@gmail.com>
2020-01-29 21:26:19 -0300

    calculo desnecessário no momento

--- functions/apoio/comissoes/apoio/calculoNovo.js
+++
@@ -1,9 +0,0 @@
-const calculoNovo = (dataCalculo,mes,ano) => {
-    for(let i = 0; i < dataCalculo.values.length; i++){
-        if(dataCalculo.values[i][0] == ano && dataCalculo.values[i][1] == mes && dataCalculo.values[i][2] == "Novo"){
-            return dataCalculo.values[i][3]
-        }
-    }
-}
-
-module.exports = calculoNovo
