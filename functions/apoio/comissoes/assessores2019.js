4f3962443cc397073c5c130518d4919408b0035e
unknown <ahmad.forhat@gmail.com>
2020-01-29 21:02:42 -0300

    implementação da função que calcula a comissão dos assessores em 2019

---
+++ functions/apoio/comissoes/assessores2019.js
@@ -0,0 +1,25 @@
+// Função de apoio para o array dado
+const limite = (arrayReceitas, porcentagemComissao, limite) => {
+    arrayReceitas.map(receita => {
+      let soma;
+      if (receita * porcentagemComissao < limite) {
+        soma += receita * porcentagemComissao;
+      } else {
+        soma += limite;
+      }
+      return soma;
+    });
+  };
+  
+  // Função de calculo das comissões dos assessores
+  const assessores2019 = (
+    receitas_mes_client_novo_afiliado,
+    receitas_mes_client_novo,
+    receitas_mes_cliente_antigo
+  ) => {
+    const baseNovo = funcaoMap(receitas_mes_client_novo, 0.4);
+    const baseNovoAfiliado = funcaoMap(receitas_mes_client_novo_afiliado, 0.2);
+    const baseAntigo = funcaoMap(receitas_mes_cliente_antigo, 0.1);
+    return baseNovo + baseNovoAfiliado + baseAntigo;
+  };
+  
