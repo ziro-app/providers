a56bdfc8f155c30e8b0d4cf7fe71514c37f79449
AhmadForhat <ahmad.forhat@gmail.com>
2020-02-05 17:33:54 -0300

    Adicionando variaveis para facilitar leitura

--- arrayToObject.js
+++ arrayToObject.js
@@ -1,32 +1,22 @@
-// error checks
-// if header.length === data[0].length
-// if values.length >= 2
-// if is not null
-
-// // Teste Condicionais
-// const conditional = {
-//   valueIsObject = typeof sheet.values == 'object'),
-//   moreThenTwoArrayLength = sheet.values.length >= 2,
-//   columnHeaderEqualrow = header.length === data[0].length
-// }
-
-// Fazer função if que transforma em numero --> brasilianDecimalToNumberEua
-
 const strBrasilianToEuaNumber = numb => {
   if (!isNaN(Number(numb.toString().replace(",", "."))))
     return Math.round(Number(numb.toString().replace(",", ".")) * 100) / 100;
   return numb;
 };
 
-const arrayToObject = (sheet) => {
+const arrayToObject = ({values}) => {
+    // Trocando o nome das variaveis e condicionais
     let respostaMenorQueDois = "A função não pode ser executada sem pelo menos apresenta um cabeçalho e um corpo"
-    let respostaTypeOf = "Values do objeto não foi encontrado"
+    let respostaTypeOf = "Values do objeto não foi encontrado ou sua resposta não é um objeto"
     let resBodyEqualHeader = "O número de colunas do cabeçalho tem que ser igual ao número de colunas do corpo"
-    if(typeof sheet.values == 'object'){
-        if(sheet.values.length >= 2){
-          const { values } = sheet;
-          const [header, ...data] = values;
-          if(header.length === data[0].length){
+    let tipoObjeto = typeof values == 'object'
+    let temCabecalhoCorpo = values.length >= 2
+    // Começo das condicionais e codigo
+    if(tipoObjeto){
+      if(temCabecalhoCorpo){
+        const [header, ...data] = values;
+        let columnEqualHeaderBody = header.length === data[0].length
+          if(columnEqualHeaderBody){
             const object = data.map(row => {
               return Object.fromEntries(
                 row.map((column, index) => {
@@ -46,7 +36,5 @@ const arrayToObject = (sheet) => {
       throw respostaTypeOf
     }
 }
-
-console.log(arrayToObject(dataSheets))
 
 module.exports = arrayToObject
