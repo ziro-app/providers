a6b7dc3633ec51341c4a93e57815c110e61961a4
AhmadForhat <ahmad.forhat@gmail.com>
2020-02-04 21:20:17 -0300

    mudando de arquivo o list resume

---
+++ functions/listResume.js
@@ -0,0 +1,25 @@
+const main = require('./apoio/main')
+require('dotenv').config()
+
+
+exports.handler = function(event, context, callback){
+    const send = body => {
+        callback(null, {
+            statusCode:200,
+            body: JSON.stringify(body)
+        })
+    }
+    const getEmployees = async () => {
+            const resultado = await main(event.queryStringParameters.mes,event.queryStringParameters.ano)
+            try {
+                send(resultado)
+            } catch (error) {
+                send(error)
+            }
+    }
+    if(event.httpMethod == 'GET' && event.headers.authorization == process.env.basicAuth){
+        getEmployees()
+    }else{
+        send("Erro na autenticação ou no metodo HTTP!")
+    }
+}
