62a37cc97e4956ad0e9ef45d4518d8f54ab9fbd6
AhmadForhat <ahmad.forhat@gmail.com>
2020-02-05 11:03:10 -0300

    Adicionando erros

--- arrayToObject.js
+++ arrayToObject.js
@@ -3,22 +3,36 @@
 // if values.length >= 2
 // if is not null
 
-const sheet = {
-  range: "Test2!A1:C1001",
-  majorDimension: "ROWS",
-  values: [
-    ["name", "id", "role"]
-  ]
-};
+const dataSheets = {values:[['nome','sobrenome','idade'],['Ahmad', 'Forhat', 24],['João', 'Berinjela',70]]}
+const arrayToObject = (sheet) => {
+  try {
+    if(typeof sheet.values == 'object'){
+      if(sheet.values.length >= 2){
+        const { values } = sheet;
+        const [header, ...data] = values;
+        if( header.length === data[0].length){
+          const object = data.map(row => {
+            return Object.fromEntries(
+              row.map((column, index) => {
+                return [header[index], row[index]];
+              })
+            )
+          })
+          return object
+        }else{
+          throw "O número de colunas do cabeçalho tem que ser igual ao número de colunas do corpo"
+        }
+      }else{
+        throw "Não pode executar a função apenas com o cabeçalho"
+      }
+    }else{
+      throw "Erro ao procurar values no objeto passado"
+    }
+  } catch (error) {
+    return error
+  }
+}
 
-const { values } = sheet;
-const [header, ...data] = values;
-const object = data.map(row => {
-  return Object.fromEntries(
-    row.map((column, index) => {
-      return [header[index], row[index]];
-    })
-  );
-});
-console.log('FROM:',values)
-console.log('TO:',object);
+console.log(arrayToObject(dataSheets))
+
+module.exports = arrayToObject
