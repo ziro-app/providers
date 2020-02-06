3f121a426909192b9c0d5eb067650f1e4e7db985
AhmadForhat <ahmad.forhat@gmail.com>
2020-02-05 23:54:28 -0300

    adicionando decimalUsa

--- arrayToObject.js
+++ arrayToObject.js
@@ -1,6 +1,6 @@
-const decimalUsa = require('decimalUsa')
+const decimalUsa = require('@ziro/decimal-usa')
 
-const arraySheetsToObject = ({values}) => {
+const arrayToObject = ({values}) => {
     // Trocando o nome das variaveis e condicionais
     let respostaMenorQueDois = "A função não pode ser executada sem pelo menos apresenta um cabeçalho e um corpo"
     let respostaTypeOf = "Values do objeto não foi encontrado ou sua resposta não é um objeto"
@@ -8,30 +8,22 @@ const arraySheetsToObject = ({values}) => {
     let tipoObjeto = typeof values == 'object'
     let temCabecalhoCorpo = values.length >= 2
     // Começo das condicionais e codigo
-    if(tipoObjeto){
-      if(temCabecalhoCorpo){
-        const [header, ...data] = values;
-        let columnEqualHeaderBody = header.length === data[0].length
-          if(columnEqualHeaderBody){
-            const object = data.map(row => {
-              return Object.fromEntries(
-                row.map((column, index) => {
-                  let numbOrString = decimalUsa(row[index])
-                  if(isNaN(numbOrString)) return [header[index], row[index]]
-                  return [header[index], numbOrString];
-                })
-              )
-            })
-            return object
-          }else{
-            throw resBodyEqualHeader
-          }
-        }else{
-          throw respostaMenorQueDois
-        }
-    }else{
-      throw respostaTypeOf
-    }
+    if(!tipoObjeto) throw respostaTypeOf
+    if(!temCabecalhoCorpo) throw respostaMenorQueDois
+    const [header, ...data] = values;
+    let columnEqualHeaderBody = header.length === data[0].length
+    if(!columnEqualHeaderBody) throw resBodyEqualHeader
+      const object = data.map(row => {
+      return Object.fromEntries(
+        row.map((column, index) => {
+          let numbOrString = decimalUsa(row[index])
+          if(isNaN(numbOrString)) return [header[index], row[index]]
+          return [header[index], numbOrString];
+        })
+      )
+    })
+      return object
+
 }
 
-module.exports = arraySheetsToObject
+module.exports = arrayToObject
