2f80b1cf69a44af0ef268e51dd8e73469574738a
Ziro VI <ahmad.forhat@gmail.com>
2020-02-07 13:54:48 -0300

    deixando somente teste da função que calcula comissão assessor2020

--- test.js
+++ test.js
@@ -1,10 +1,20 @@
 const test = require('ava')
-// const assessor2020 = require('./functions/apoio/comissoes/asessores2020')
-// const assessores2019 = require('./functions/apoio/comissoes/assessores2019')
-// const cobranca2019 = require('./functions/apoio/comissoes/cobranca2019')
-// const lojistica2019 = require('./functions/apoio/comissoes/lojistica2019')
-// const prospeccao2019 = require('./functions/apoio/comissoes/prospeccao2020')
-const arrayObject = require('@ziro/array-object')
+const assessores2020 = require('./functions/apoio/comissoes/assessores2020')
+
+const dataSheets = [
+    { ano: 2020, mes: 12, assessor: 'David', valor: 80000, receita: 2000.12 },
+    { ano: 2020, mes: 12, assessor: 'David', valor: 40000, receita: 3000.76 },
+    { ano: 2020, mes: 12, assessor: 'David', valor: 50000, receita: 2000.56 },
+    { ano: 2020, mes: 12, assessor: 'David', valor: 25000, receita: 4000.0 },
+    { ano: 2020, mes: 12, assessor: 'João', valor: 36000, receita: 4000.0 }
+]
+
+test('Teste comissão assessor 2020', t => {
+    const value = assessores2020(dataSheets,2020,12,'David')
+    const arrayObjectCalculated = value
+    const arrayObjectExpected = 19500
+    t.deepEqual(arrayObjectCalculated, arrayObjectExpected)
+})
 
 // const dados = {
 //     receita_mes_ziro: 150000,
@@ -16,13 +26,6 @@ const arrayObject = require('@ziro/array-object')
 //     cobrancas_mes: 100000,
 //     transacao_mes_afiliado: 200000
 // }
-
-// test('Comissão Assessor 2020', t => {
-//     const value = assessor2020(dados.transacionado_mes, dados.receita_mes)
-//     const comissionCalculated = value
-//     const comissionExpected = 3600
-//     t.is(comissionCalculated, comissionExpected)
-// })
 
 // test('Comissão Assessores 2019', async t => {
 //     const value = assessores2019(dados.receitas_mes_novo_cliente,dados.receita_mes_novo_afiliado,dados.receita_mes_antigo_afiliado)
@@ -53,12 +56,3 @@ const arrayObject = require('@ziro/array-object')
 //     const comissionExpected = 6500
 //     t.is(comissionCalculated, comissionExpected)
 // })
-
-const dataSheets = {values:[['nome','sobrenome','idade'],['Ahmad', 'Forhat', '24'],['João', 'Berinjela','70']]}
-
-test('arrayObject', t => {
-    const value = arrayObject(dataSheets)
-    const arrayObjectCalculated = value
-    const arrayObjectExpected = [{nome: 'Ahmad', sobrenome : 'Forhat', idade: 24 }, {nome: 'João', sobrenome : 'Berinjela', idade: 70}]
-    t.deepEqual(arrayObjectCalculated, arrayObjectExpected)
-})
