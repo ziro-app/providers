d7e8ffd102275533f6a90f788a165191e7414c34
Ziro VI <ahmad.forhat@gmail.com>
2020-02-07 15:44:15 -0300

    função reduce para somas receitas mês ziro

--- functions/apoio/comissoes/lojistica2019.js
+++ functions/apoio/comissoes/lojistica2019.js
@@ -1,6 +1,26 @@
 //Lojistica - começou a ter comissão 09/2019
-const lojistica2019 = receita_mes_ziro => {
+const calculoLojistica2019 = receita_mes_ziro => {
     return receita_mes_ziro * 0.005
 }
+
+// Agrupamento dos valores necessários para o calculo
+const reduceBaseBoletos = (baseComissoes, ano, mes) => {
+    const filtrado = baseComissoes.filter(item => {
+        const condicao =
+        item.ano === ano && item.mes === mes
+        return condicao
+    })
+    const receitas = filtrado.map(item => item.receita)
+    const somaReceitas = receitas.reduce(
+        (anterior, proximo) => anterior + proximo
+    )
+    return somaReceitas
+}
   
+// Função "main"
+const lojistica2019 = (baseComissoes, ano, mes) => {
+    const receita_mes_ziro = reduceBaseBoletos(baseComissoes, ano, mes)
+    return calculoLojistica2019(receita_mes_ziro)
+}
+
 module.exports = lojistica2019
