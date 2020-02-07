00d5a797670f5979eb087d13fe9ca60909e7704c
Ziro VI <ahmad.forhat@gmail.com>
2020-02-07 17:33:02 -0300

    adicionando função de agrupamento nas cobranças

--- functions/apoio/comissoes/cobranca2019.js
+++ functions/apoio/comissoes/cobranca2019.js
@@ -1,7 +1,25 @@
 //Cobranca começou a ter comissão 09/2019
-const cobranca2019 = cobrancas_mes => {
+const calculoCobranca2019 = cobrancas_mes => {
     return cobrancas_mes * 0.007
 }
+
+const reduceBaseBoletos = (baseComissoes, ano, mes) => {
+    const filtrado = baseComissoes.filter(item =>
+        item.anoRecebido === ano && item.mesRecebido === mes
+    )
+    const recebido = filtrado.map(item => item.recebido)
+    const somaRecebido = recebido.reduce(
+        (anterior, proximo) => anterior + proximo
+    )
+    return somaRecebido
+}
+  
+// Função "main"
+const cobranca2019 = (baseComissoes, ano, mes) => {
+    const receita_mes_ziro = reduceBaseBoletos(baseComissoes, ano, mes)
+    return calculoCobranca2019(receita_mes_ziro)
+}
+
   
 module.exports = cobranca2019
   
