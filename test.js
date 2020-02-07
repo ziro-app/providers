8f2dc9d7ca25e610d894d4e5a8fe57fa6f09a9fa
Ziro VI <ahmad.forhat@gmail.com>
2020-02-07 15:45:18 -0300

    teste dos reduce da função

--- test.js
+++ test.js
@@ -1,54 +1,25 @@
 const test = require('ava')
-const assessoria2020 = require('./functions/apoio/comissoes/assessoria2020')
-const assessoria2019 = require('./functions/apoio/comissoes/assessoria2019')
-const cobranca2019 = require('./functions/apoio/comissoes/cobranca2019')
+const assessores2020 = require('./functions/apoio/comissoes/assessores2020')
 const lojistica2019 = require('./functions/apoio/comissoes/lojistica2019')
-const prospeccao2019 = require('./functions/apoio/comissoes/prospeccao2020')
 
-const dados = {
-    receita_mes_ziro: 150000,
-    receita_mes: 24000,
-    transacionado_mes: 300000,
-    receitas_mes_novo_cliente:[200,3000,4000],
-    receita_mes_novo_afiliado: 15000,
-    receita_mes_antigo_afiliado: 25000,
-    cobrancas_mes: 100000,
-    transacao_mes_afiliado: 200000
-}
+const dataSheets = [
+    { ano: 2020, mes: 12, assessor: 'David', valor: 80000, receita: 2000.12 },
+    { ano: 2020, mes: 12, assessor: 'David', valor: 40000, receita: 3000.76 },
+    { ano: 2020, mes: 12, assessor: 'David', valor: 50000, receita: 2000.56 },
+    { ano: 2020, mes: 12, assessor: 'David', valor: 25000, receita: 4000.0 },
+    { ano: 2020, mes: 12, assessor: 'João', valor: 36000, receita: 4000.0 }
+]
 
-test('Comissão Assessoria 2020', t => {
-    const value = assessoria2020(dados.transacionado_mes, dados.receita_mes)
-    const comissionCalculated = value
-    const comissionExpected = 3600
-    t.is(comissionCalculated, comissionExpected)
+test('Comissão assessor 2020', t => {
+    const value = assessores2020(dataSheets,2020,12,'David')
+    const arrayObjectCalculated = value
+    const arrayObjectExpected = 19500
+    t.deepEqual(arrayObjectCalculated, arrayObjectExpected)
 })
 
-test('Comissão Assessoria 2019', async t => {
-    const value = assessoria2019(dados.receitas_mes_novo_cliente,dados.receita_mes_novo_afiliado,dados.receita_mes_antigo_afiliado)
+test('Comissão Lojistica 2019', async t => {
+    const value = lojistica2019(dataSheets,2020,12)
     const comissionCalculated = value
-    console.log(value)
-    const comissionExpected = 7580
-    t.is(comissionCalculated, comissionExpected)
-})
-
-test('Comissão Cobranças 2019', async t => {
-    const value = cobranca2019(dados.cobrancas_mes)
-    const comissionCalculated = value
-    const comissionExpected = 700
-    t.is(comissionCalculated, comissionExpected)
-})
-
-
-test('Comissão Lojistica 2019', t => {
-    const value = lojistica2019(dados.receita_mes_ziro)
-    const comissionCalculated = value
-    const comissionExpected = 750
-    t.is(comissionCalculated, comissionExpected)
-})
-
-test('Comissão Prospecção 2020', t => {
-    const value = prospeccao2019(dados.transacao_mes_afiliado, dados.receita_mes_novo_afiliado, dados.receita_mes_antigo_afiliado)
-    const comissionCalculated = value
-    const comissionExpected = 6500
+    const comissionExpected = 75.0072
     t.is(comissionCalculated, comissionExpected)
 })
