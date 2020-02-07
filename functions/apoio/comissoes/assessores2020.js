36419683d66045a23fada70aabe4798ace67db7d
Ziro VI <ahmad.forhat@gmail.com>
2020-02-07 13:56:27 -0300

    group by interno para tratativa de dados

---
+++ functions/apoio/comissoes/assessores2020.js
@@ -0,0 +1,42 @@
+// Segunda comissão dos assessores:
+// Comissão é de 10% sobre a receita da venda e bonus conforme a quantidade vendida conforme a tabela abaixo:
+//100000	500
+//200000	900
+//300000	1200
+//400000	1400
+
+const calculoAssessor2020 = (transacionado_mes, receita_mes) => {
+    const base = receita_mes * 0.1
+    if (transacionado_mes >= 400000) return base + 1400
+    if (transacionado_mes >= 300000) return base + 1200
+    if (transacionado_mes >= 200000) return base + 900
+    if (transacionado_mes >= 100000) return base + 500
+    else return base
+}
+
+const reduceBaseBoletos = (baseComissoes, ano, mes, assessor) => {
+    const filtrado = baseComissoes.filter(item => {
+        const condicao =
+        item.ano === ano && item.mes === mes && item.assessor === assessor
+        return condicao
+    })
+    const receitas = filtrado.map(item => item.receita)
+    const transacaoes = filtrado.map(item => item.valor)
+    const somaTransacoes = transacaoes.reduce(
+        (anterior, proximo) => anterior + proximo
+    )
+    const somaReceitas = receitas.reduce(
+        (anterior, proximo) => anterior + proximo
+    )
+    const somas = { transacoes: somaTransacoes, receitas: somaReceitas }
+    return somas
+}
+  
+
+const assessor2020 = (baseComissoes, ano, mes, assessor) => {
+    const receita_mes = reduceBaseBoletos(baseComissoes, ano, mes, assessor).receitas
+    const transacionado_mes = reduceBaseBoletos(baseComissoes, ano, mes, assessor).transacoes
+    return calculoAssessor2020(receita_mes, transacionado_mes)
+}
+
+module.exports = assessor2020
