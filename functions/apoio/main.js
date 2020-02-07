59b869310ada49c84b5854d2f0c832c6c2e759de
Ziro VI <ahmad.forhat@gmail.com>
2020-02-07 15:32:31 -0300

    adicionando lojistica2019

--- functions/apoio/main.js
+++ functions/apoio/main.js
@@ -2,13 +2,15 @@ const rp = require('request-promise-native');
 const optionsGet = require('./googlesheets/optionsGetGoogle')
 const arrayObject = require('@ziro/array-object')
 const assessor2020 = require('./comissoes/assessores2020')
+// const lojistica2019 = require('./comissoes/lojistica2019')
 
 const main = async (ano, mes, assessor) => {
     const dataBaseSheets = await rp(optionsGet("'Base'!A:L"))
     const baseComissoes = await arrayObject(dataBaseSheets)
-    const result = await assessor2020(baseComissoes,ano,mes,assessor)
+    const resultAssessor2020 = await assessor2020(baseComissoes,ano,mes,assessor)
+    // const resultLojistica2019 = await lojistica2019(baseComissoes,ano,mes)
     try {
-        return result
+        return resultAssessor2020
     } catch (error) {
         return error
     }
