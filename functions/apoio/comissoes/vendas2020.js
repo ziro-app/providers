42c3f8aa2799f5a6736b852683caebc15dd43fc1
unknown <ahmad.forhat@gmail.com>
2020-02-10 15:30:35 -0300

    arrumando erros da função de vendas

--- functions/apoio/comissoes/vendas2020.js
+++ functions/apoio/comissoes/vendas2020.js
@@ -1,6 +1,6 @@
 // Prospecção
 // Função atual recebe 1000 se tiver mais de 100000 de transação começou 01/2020
-const calculoProspeccao2020 = (
+const calculoVendas2020 = (
     transacao_mes_afiliado,
     receita_mes_novo_afiliado,
     receita_mes_antigo_afiliado
@@ -15,7 +15,7 @@ const calculoProspeccao2020 = (
 // Agrupamento dos valores necessários para o calculo
 const transacoes_e_receitas = (baseComissoes, ano, mes, afiliado, tipoCliente) => {
     const filtrado = baseComissoes.filter(item =>
-        item.ano === ano && item.mes === mes && item.afiliado === afiliado &&item.tipocliente === tipoCliente
+        item.ano === ano && item.mes === mes && item.afiliado === afiliado && item.tipoCliente === tipoCliente
     )
     const receitas = filtrado.map(item => item.receita)
     const transacoes = filtrado.map(item => item.valor)
@@ -25,15 +25,20 @@ const transacoes_e_receitas = (baseComissoes, ano, mes, afiliado, tipoCliente) =
     const somaReceitas = receitas.reduce(
         (anterior, proximo) => anterior + proximo
     )
-    return { transacao_mes_afiliado_novo: somaTransacoes, receita_mes_novo_afiliado: somaReceitas }
+    console.log({ transacao_mes_afiliado: somaTransacoes, receita_mes_afiliado: somaReceitas })
+    return { transacao_mes_afiliado: somaTransacoes, receita_mes_afiliado: somaReceitas }
 }
   
 // Função "main"
 const vendas2020 = (baseComissoes, ano, mes, afiliado) => {
-    const { transacao_mes_afiliado_novo, receita_mes_novo_afiliado } = transacoes_e_receitas(baseComissoes, ano, mes, afiliado, 'Novo')
-    const { transacao_mes_afiliado_antigo, receita_mes_antigo_afiliado } = transacoes_e_receitas(baseComissoes, ano, mes, afiliado, 'Antigo')
+    const receitaTransacoesNovo = transacoes_e_receitas(baseComissoes, ano, mes, afiliado, 'Novo')
+    const receitaTransacoesAntigo = transacoes_e_receitas(baseComissoes, ano, mes, afiliado, 'Antigo')
+    const transacao_mes_afiliado_novo = receitaTransacoesNovo.transacao_mes_afiliado
+    const transacao_mes_afiliado_antigo = receitaTransacoesAntigo.transacao_mes_afiliado
     const transacao_mes_afiliado = transacao_mes_afiliado_novo + transacao_mes_afiliado_antigo
-    return calculoProspeccao2020(transacao_mes_afiliado, receita_mes_novo_afiliado, receita_mes_antigo_afiliado)
+    const receita_mes_novo_afiliado = receitaTransacoesNovo.receita_mes_afiliado
+    const receita_mes_antigo_afiliado = receitaTransacoesAntigo.receita_mes_afiliado
+    return calculoVendas2020(transacao_mes_afiliado, receita_mes_novo_afiliado, receita_mes_antigo_afiliado)
 }
   
 module.exports = vendas2020
