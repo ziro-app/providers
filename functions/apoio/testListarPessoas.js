90dc70f48ab6405e315c28c625600a4c8140b869
unknown <ahmad.forhat@gmail.com>
2020-02-12 15:44:57 -0200

    funções de apoio para o calculo

--- functions/apoio/testListarPessoas.js
+++ functions/apoio/testListarPessoas.js
@@ -3,6 +3,17 @@ const arrayObject = require('@ziro/array-object')
 const optionsGet = require('./googlesheets/optionsGetGoogle')
 require('dotenv').config()
 
+const transData = (dataSheets) => {
+    const arrayDate = dataSheets.split('-')
+    const data = new Date(arrayDate[0], arrayDate[1], arrayDate[2])
+    return data
+}
+
+const calculoUltimoDia = (ano,mes) => {
+    const finalMes = new Date((new Date(ano, mes))-1)
+    return finalMes
+}
+
 let result = []
 const listarApelidos = (basePessoas) => {
     basePessoas.map(item => {
