8f73c7b8164fff0ece78821e06b6e4ed0726ac35
unknown <ahmad.forhat@gmail.com>
2020-02-12 16:13:38 -0200

    trocando as variaveis

--- functions/apoio/testListarPessoas.js
+++ functions/apoio/testListarPessoas.js
@@ -17,9 +17,9 @@ const calculoUltimoDia = (ano,mes) => {
 let result = []
 const listarApelidos = (base, ano, mes) => {
     const entrouNesseMes = base.filter(item => transData(item.dataInicio) > new Date(ano,mes))
-    entrouNesseMes.map(item => result.push({apelido: item.apelido, escopo:item.escopo, parcela1:item.parcela1*(calculoUltimoDia(ano,mes).getDate() - transData(item.dataInicio).getDate())/calculoUltimoDia(ano,mes).getDate()}))
+    entrouNesseMes.map(item => result.push({apelido: item.apelido, escopo:item.escopo, ano:ano, mes:mes, parcela1:item.parcela1*(calculoUltimoDia(ano,mes).getDate() - transData(item.dataInicio).getDate())/calculoUltimoDia(ano,mes).getDate()}))
     const ativoNesseMes = base.filter(item => transData(item.dataInicio) < new Date(ano,mes) && item.dataFim === '-' || transData(item.dataInicio) < new Date(ano,mes) && transData(item.dataFim) < new Date(ano,mes))
-    ativoNesseMes.map(item => result.push({apelido:item.apelido, escopo:item.escopo, parcela1:item.parcela1}))
+    ativoNesseMes.map(item => result.push({apelido:item.apelido, escopo:item.escopo, ano:ano, mes:mes, parcela1:item.parcela1}))
     return result
 }
 
