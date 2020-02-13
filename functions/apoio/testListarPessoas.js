2f1dd89a02ee925b32fa0f15dfa9eb55654cde60
unknown <ahmad.forhat@gmail.com>
2020-02-13 13:02:47 -0200

    functionality with 12 months

--- functions/apoio/testListarPessoas.js
+++ functions/apoio/testListarPessoas.js
@@ -8,7 +8,18 @@ const testeListarPessoas = async () => {
     try {
         const dataBasePessoas = await rp(optionsGet('Pessoas!A:V'))
         const basePessoas = await arrayObject(dataBasePessoas)
-        console.log(listarApelidos(basePessoas,2020,1))
+        listarApelidos(basePessoas,2020,1)
+        listarApelidos(basePessoas,2020,2)
+        listarApelidos(basePessoas,2020,3)
+        listarApelidos(basePessoas,2020,4)
+        listarApelidos(basePessoas,2020,5)
+        listarApelidos(basePessoas,2020,6)
+        listarApelidos(basePessoas,2020,7)
+        listarApelidos(basePessoas,2020,8)
+        listarApelidos(basePessoas,2020,9)
+        listarApelidos(basePessoas,2020,10)
+        listarApelidos(basePessoas,2020,11)
+        console.log(listarApelidos(basePessoas,2020,12))
     } catch (error) {
         console.log(error)
     }
