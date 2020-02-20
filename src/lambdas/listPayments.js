e096be6286cf85b6c97bccebd4b7deb65c460dc1
unknown <ahmad.forhat@gmail.com>
2020-02-20 01:27:18 -0300

    Adicionando função padrão netlify

---
+++ src/lambdas/listPayments.js
@@ -0,0 +1,34 @@
+const rp = require('request-promise-native')
+const arrayObject = require('@ziro/array-object')
+const listaParcela = require('../apoio/listarParcelas')
+const optionsBatchGet = require('../apoio/googlesheets/optionsbatchGet')
+require('dotenv').config()
+
+
+exports.handler = function(event, context, callback){
+    const send = body => {
+        callback(null, {
+            statusCode:200,
+            body: JSON.stringify(body)
+        })
+    }
+    const getEmployees = async () => {
+        try {
+            const results = await rp(optionsBatchGet(['Base Comissões!A:Q','Apoio Comissões Assessores 2019!A:H','Pessoas!A:V', 'Reajustes!A:G']))
+            const [dataBaseSheets,dataAssessores,dataBasePessoas, dataBaseReajustes] = results.valueRanges 
+            const baseComissoes = arrayObject(dataBaseSheets)
+            const baseAssessores = arrayObject(dataAssessores)
+            const basePessoas = arrayObject(dataBasePessoas)
+            const baseReajustes = arrayObject(dataBaseReajustes)
+            const parcelas2 = listaParcela(basePessoas, baseComissoes, baseAssessores, baseReajustes)
+            send(parcelas2)
+        } catch (error) {
+            send(error)
+        }
+    }
+    if(event.httpMethod == 'GET' && event.headers.authorization == process.env.basicAuth){
+        getEmployees()
+    }else{
+        send('Erro na autenticação ou no metodo HTTP!')
+    }
+}
