8b1af00dcea0f788332c337f6ee1a856cd3329fc
unknown <ahmad.forhat@gmail.com>
2020-02-28 12:48:31 -0300

    modificações necessárias para a mudança do importrange

--- src/apoio/parcela2.js
+++ src/apoio/parcela2.js
@@ -7,11 +7,11 @@ const vendas2020 = require('./comissoes/vendas2020')
 // Condicional de chamada da função de parcela2
 const listarParcela2 = (modeloParcela2, baseComissoes, baseAssessor, ano,mes,apelido) => {
     if(modeloParcela2 === 'nenhum') return 0
-    if(modeloParcela2 === 'assessor2020') return assessor2020(baseComissoes, ano, mes, apelido)
-    if(modeloParcela2 === 'assessor2019') return assessor2019(baseAssessor, ano, mes, apelido)
+    if(modeloParcela2 === 'assessoria2020') return assessor2020(baseComissoes, ano, mes, apelido)
+    if(modeloParcela2 === 'assessoria2019') return assessor2019(baseAssessor, ano, mes, apelido)
     if(modeloParcela2 === 'cobranca2019') return cobranca2019(baseComissoes, ano, mes)
-    if(modeloParcela2 === 'lojistica2019') return logistica2019(baseComissoes, ano, mes)
-    if(modeloParcela2 === 'vendas2020') return vendas2020(baseComissoes, ano, mes, apelido)
+    if(modeloParcela2 === 'logistica2019') return logistica2019(baseComissoes, ano, mes)
+    if(modeloParcela2 === 'vendas2019') return vendas2020(baseComissoes, ano, mes, apelido)
 }
 
 module.exports = listarParcela2
