d9ab64948b21c5be2646b699b005c2d70d7f1a3e
unknown <ahmad.forhat@gmail.com>
2020-03-05 11:58:42 -0300

    implementando mais testes para reajuste

--- src/apoio/testeReajustes.js
+++ src/apoio/testeReajustes.js
@@ -1,6 +1,6 @@
 const reajustes = require('./searchReajuste')
 
-const base = [{cadastro:'0b8d5c2787b12438e31ce559cd173fd5', apelido:'João', data:'01/03/2020',escopo:'Dev',parcela1:4000, modeloParcela2:'-'}]
+const base = [{cadastro:'0b8d5c2787b12438e31ce559cd173fd5', apelido:'João', data:'01/01/2020',escopo:'Dev',parcela1:2500, modeloParcela2:'Assessor2020'},{cadastro:'0b8d5c2787b12438e31ce559cd173fd5', apelido:'Bernardo', data:'01/01/2020',escopo:'Dev',parcela1:10000, modeloParcela2:'Banana'},{cadastro:'0b8d5c2787b12438e31ce559cd173fd5', apelido:'João', data:'01/03/2020',escopo:'Dev',parcela1:4000, modeloParcela2:'-'}]
 
 const testeCoef = (expected, titulo, apelido,dataProcurada,baseReajuste) => {
     const simulado = reajustes(apelido, dataProcurada, baseReajuste)
@@ -14,7 +14,12 @@ const testeCoef = (expected, titulo, apelido,dataProcurada,baseReajuste) => {
 }
 
 testeCoef([4000,'Dev'], 'Teste calculo com função completa', 'João',new Date(2020,3),base)
+testeCoef([2500,'Assessor2020','Dev'], 'Teste calculo com função completa 2', 'João',new Date(2020,1),base)
+testeCoef([10000,'Banana','Dev'], 'Teste calculo com função completa 3', 'Bernardo',new Date(2020,1),base)
 testeCoef([], 'Teste calculo com função nome sem nada na lista', 'Pedro',new Date(2020,3),base)
 testeCoef([], 'Teste calculo com data em formato errado', 'João','20/12/2020',base)
 testeCoef([], 'Teste calculo com data em formato errado2', 'João','banana',base)
 testeCoef([], 'Teste calculo com base vazia', 'João',new Date(2020,3),[])
+testeCoef([], 'Teste calculo com base NaN', 'João',new Date(2020,3),NaN)
+testeCoef([], 'Teste calculo com base undefined', 'João',new Date(2020,3),undefined)
+testeCoef([], 'Teste calculo com base zero', 'João',new Date(2020,3),0)
