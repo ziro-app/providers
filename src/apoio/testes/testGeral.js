5c2dbe0c578d5027389b2a531a5883cf7557effa
unknown <ahmad.forhat@gmail.com>
2020-03-12 14:56:05 -0300

    teste geral de comparação de objetos

---
+++ src/apoio/testes/testGeral.js
@@ -0,0 +1,35 @@
+const rp = require('request-promise-native')
+const arrayObject = require('@ziro/array-object')
+const listaParcela = require('../listarParcelas')
+const optionsBatchGet = require('../googlesheets/optionsbatchGet')
+require('dotenv').config()
+
+function getAllIndexes(arr, val) {
+    var indexes = [], i = -1
+    while ((i = arr.indexOf(val, i+1)) != -1){
+        indexes.push(i)
+    }
+    return indexes
+}
+
+const getEmployees = async () => {
+    const requests = await rp(optionsBatchGet(['Base Comissões!A:Q','Apoio Comissões Assessores 2019!A:H','Pessoas!A:V', 'Reajustes!A:G','Valor Pago!A:N']))
+    const [dataBaseSheets,dataAssessores,dataBasePessoas, dataBaseReajustes, dataPago] = requests.valueRanges 
+    const baseComissoes = arrayObject(dataBaseSheets)
+    const baseAssessores = arrayObject(dataAssessores)
+    const basePessoas = arrayObject(dataBasePessoas)
+    const baseReajustes = arrayObject(dataBaseReajustes)
+    const pago = arrayObject(dataPago)
+    const simulado = listaParcela(basePessoas, baseComissoes, baseAssessores, baseReajustes)
+    const simuladoFlat = simulado.flat()
+    // console.log('Simulado',simuladoFlat)
+    // console.log('Pago', pago)
+    const arrayResposta = simuladoFlat.map(item => pago.find(correspondente => {
+        return item.mes === correspondente.mes && item.ano === correspondente.ano && item.apelido === correspondente.apelido && item.parcela1 === correspondente.parcela1 && item.parcela2 === correspondente.parcela2
+    }))
+    const indexErros = getAllIndexes(arrayResposta,undefined)
+    const erros = indexErros.map(item => simuladoFlat[item])
+    console.log(erros)
+}
+
+getEmployees()
